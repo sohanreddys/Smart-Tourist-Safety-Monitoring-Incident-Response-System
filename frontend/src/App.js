1ead59e0059eff5f27a498754fe5c950efb9f0fd
Sohan Reddy Seelam <sohanreddyseelamcbit@gmail.com>
2026-04-08 22:50:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
